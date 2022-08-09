610d91608b95b0042518a9675d78fceb2c9400f1
Rodrigo Behrmann <54535102+rodrigoBehrmann@users.noreply.github.com>
2022-08-09 14:37:46 -0300

    Update taskAtHand.js

--- taskAtHand.js
+++ taskAtHand.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function MyApp(){
+function TaskAtHandApp(){
     var version = "1.0";
 
     function setStatus(message){
@@ -8,12 +8,34 @@ function MyApp(){
     }
 
     this.start = function(){
+        $("#new-task-name").keypress(function(e){
+            if(e.which == 13){
+                addTask();
+                return false;
+            }
+        })
+        .focus();
+        
         $("#app>header").append(version);
         setStatus("ready");
     };
 }
 
 $(function(){
-    window.app = new MyApp();
+    window.app = new TaskAtHandApp();
     window.app.start();
 })
+
+function addTask(){
+    var taskName = $("#new-task-name").val();
+    if(taskName){
+        addTaskElement(taskName);
+        $("#new-task-name").val("").focus;
+    }
+}
+
+function addTaskElement(taskName){
+    var $task = $("<li></li>");
+    $task.text(taskName);
+    $("#task-list").append($task);
+}
